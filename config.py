0cc4184259aee4e26bf835d8ebec7e659efd65d1
aryanmybhatija <sonutomarlucky@gmail.com>
2025-06-02 12:19:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24602445"
-api_hash = "180adef97e5d2f8a246f59280ce625b5"
-bot_token = "7755241058:AAEHWU_el9L_YT7IutYOG0R9hEadiy2eIf4"
+api_id = ""
+api_hash = ""
+bot_token = ""
 auth_users = [6890400066]
